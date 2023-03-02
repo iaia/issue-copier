2abb312594dd8d8de701d23a66553bb0cdf49c4a
iaia <iaia72160@gmail.com>
2023-03-02 13:44:15 +0900

    Update

--- main.ts
+++ main.ts
@@ -1,9 +1,20 @@
 import * as core from '@actions/core';
+import * as github from '@actions/github';
 
-async function run(): Promise<void> {
+async function run() {
     try {
-        const nameToGreet = core.getInput('who-to-greet');
-        console.log(`Hello ${nameToGreet}!`);
+        const githubToken = core.getInput('github access token', { required: true })
+        const octokit = github.getOctokit(githubToken)
+
+        octokit.rest.issues.listForRepo({
+            owner: 'iaia',
+            repo: 'issue-copier',
+            assignee: 'none'
+        }).then((res) => {
+            res.data.forEach((v, index, array) => {
+                core.debug(`unassigned issue: title: ${v.title} #${v.number}, ${v.created_at}`);
+            })
+        })
     } catch (error) {
         if (error instanceof Error) core.setFailed(error.message)
     }
