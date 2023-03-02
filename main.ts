1a443dca042d2d2497b1ed64e9a51e7ef390284f
iaia <iaia72160@gmail.com>
2023-03-02 11:12:57 +0900

    Add

--- main.ts
+++ main.ts
@@ -2,10 +2,8 @@ import * as core from '@actions/core';
 
 async function run(): Promise<void> {
     try {
-        const ms: string = core.getInput('milliseconds')
-        core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-        core.setOutput('time', new Date().toTimeString())
+        const nameToGreet = core.getInput('who-to-greet');
+        console.log(`Hello ${nameToGreet}!`);
     } catch (error) {
         if (error instanceof Error) core.setFailed(error.message)
     }
